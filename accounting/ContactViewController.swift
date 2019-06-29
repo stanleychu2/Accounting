4bf8d6a602a4cedc9d69655bc39ea98ff86145e0
benbenhello <benben5514@gmail.com>
2019-06-29 23:50:15 +0800

    contact view

--- accounting/ContactViewController.swift
+++ accounting/ContactViewController.swift
@@ -112,7 +112,7 @@ class ContactViewController: UIViewController, UICollectionViewDelegate, UIColle
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ContactCollectionViewCell
         // Configure the cell
         cell.layer.cornerRadius = 3
-        cell.cellLabel.text = people[36 * pagesIndex + indexPath.row].name
+        cell.cellLabel.text = people[36 * pagesIndex + indexPath.row].company
         cell.backgroundColor = #colorLiteral(red: 0.9024838209, green: 0.6174634099, blue: 0.1791247427, alpha: 1)
         
         return cell
